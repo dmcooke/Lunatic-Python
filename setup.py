f4d17073ef5357676da83e2cc154c70a4b10ee53
David M. Cooke <david.m.cooke@gmail.com>
2009-05-02 18:04:18 -0700

    Update setup.py - remove building of 'lua-python' extension. It was an exact copy of   of the 'lua' extension? - top-level variabls LUA_LIBS, LUA_INCDIR, LUA_LIBDIR that'll need to be   edited on a site-to-site basis. Currently set to use /opt/local.

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python
 from distutils.core import setup, Extension
-from distutils.sysconfig import get_python_lib, get_python_version
 import os
 
 if os.path.isfile("MANIFEST"):
     os.unlink("MANIFEST")
 
 # You may have to change these
-PYLIBS = ["python"+get_python_version(), "pthread", "util"]
-PYLIBDIR = [get_python_lib(standard_lib=True)+"/config"]
-LUALIBS = ["lua", "lualib"]
-LUALIBDIR = []
+LUA_LIBS = ["lua"]
+LUA_INCDIR = ["/opt/local/include"]
+LUA_LIBDIR = ["/opt/local/lib"]
 
 setup(name="lunatic-python",
       version = "1.0",
@@ -27,17 +25,10 @@ Python, Python inside Lua, Lua inside Python inside Lua, Python inside Lua
 inside Python, and so on.
 """,
       ext_modules = [
-                     Extension("lua-python",
-                               ["src/pythoninlua.c", "src/luainpython.c"],
-                               library_dirs=PYLIBDIR,
-                               libraries=PYLIBS,
-                               extra_compile_args=["-rdynamic"],
-                               extra_link_args=["-rdynamic"]),
                      Extension("lua",
                                ["src/pythoninlua.c", "src/luainpython.c"],
-                               library_dirs=LUALIBDIR,
-                               libraries=LUALIBS,
-                               extra_compile_args=["-rdynamic"],
-                               extra_link_args=["-rdynamic"]),
+                               include_dirs=LUA_INCDIR,
+                               library_dirs=LUA_LIBDIR,
+                               libraries=LUA_LIBS),
                     ],
       )
