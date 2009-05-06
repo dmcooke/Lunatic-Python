2edcdf9869486c38336b7a351f7c1e59616c242c
David M. Cooke <david.m.cooke@gmail.com>
2009-05-05 19:39:57 -0700

    Initial test suite. Currently copy of the "Lua inside Python" examples from the manual, as a doctest.

---
+++ tests/test_lua.py
@@ -0,0 +1,53 @@
+"""
+>>> import lua
+>>> lg = lua.globals()
+>>> lg.string
+<Lua table at 0x...>
+>>> lg.string.lower
+<Lua function at 0x...>
+>>> lg.string.lower("Hello world!")
+'hello world!'
+
+>>> d = {}
+>>> lg.d = d
+>>> lua.execute("d['key'] = 'value'")
+>>> d
+{'key': 'value'}
+
+>>> d2 = lua.eval("d")
+>>> d is d2
+True
+
+>>> lua.eval("python")
+<Lua table at 0x...>
+
+>>> class MyClass: pass
+...
+>>> obj = MyClass()
+>>> obj
+<__main__.MyClass instance at 0x...>
+>>> lua.eval(\"\"\"python.eval('lua.eval('''python.eval("obj")''')')\"\"\")
+<__main__.MyClass instance at 0x...>
+
+>>> lua.execute("pg = python.globals()")
+>>> lua.eval("pg.obj")
+<__main__.MyClass instance at 0x...>
+
+>>> table = lua.eval("table")
+>>> def show(key, value):
+...   print "key is %s and value is %s" % (`key`, `value`)
+... 
+>>> t = lua.eval("{a=1, b=2, c=3}")
+>>> table.foreach(t, show)
+key is 'a' and value is 1
+key is 'c' and value is 3
+key is 'b' and value is 2
+>>> 
+
+"""
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(optionflags=doctest.ELLIPSIS)
+
+
